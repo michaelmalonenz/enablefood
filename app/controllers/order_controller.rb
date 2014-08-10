d9af30cc1fdf2173d6533ef4fe32ef00fed300de
Michael Malone <michael.j.b.malone@gmail.com>
2014-08-10 13:28:04 +1200

    Added the Order framework

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,40 @@
+class OrderController < ApplicationController
+
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @order = Order.new()
+    @owner = :current_user
+    #how do I decide which meal?
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save()
+      redirect_to @order
+    else
+      render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:description, :price)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805215955) do
+ActiveRecord::Schema.define(version: 20140810011424) do
 
   create_table "meals", force: true do |t|
     t.string   "title"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20140805215955) do
     t.integer "meal_id"
     t.integer "user_id"
   end
+
+  create_table "orders", force: true do |t|
+    t.decimal  "cost"
+    t.string   "description"
+    t.integer  "meal_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "orders", ["meal_id"], name: "index_orders_on_meal_id"
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
