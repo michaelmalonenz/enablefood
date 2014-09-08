a2bb769bfb6dcc21b0a9447b57cecc2ead1283b8
Michael Malone <michael.malone@taitradio.com>
2014-09-09 08:14:01 +1200

    minor tweaks to actually get the thing running from a clean start

--- db/migrate/20140608094407_associate_users_and_meals.rb
+++ db/migrate/20140608094407_associate_users_and_meals.rb
@@ -1,8 +1,8 @@
 class AssociateUsersAndMeals < ActiveRecord::Migration
   def change
     create_table :meals_users, :id => false do |t|
-      t.belongs_to :meals
-      t.belongs_to :users
+      t.belongs_to :meal
+      t.belongs_to :user
     end
   end
 end
