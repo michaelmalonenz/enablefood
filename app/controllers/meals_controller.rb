ba4148410055d313e7bc8f49394ce18924d8ab00
Michael Malone <michael.j.b.malone@gmail.com>
2014-11-06 19:07:39 +1300

    Sort the users by email address and remove a couple more references to Fat Friday

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,7 +2,7 @@ class MealsController < ApplicationController
   before_action :set_meal, only: [:show, :edit, :update, :destroy, :close_orders, :orders]
 
   def new
-    @users = User.all
+    @users = User.all.sort_by {|u| u.email}
     @meal = Meal.new do |m|
       m.users = @users
     end
